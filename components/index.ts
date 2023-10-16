b9746d2ef03c2a5c90fe35dfb620388eb14f5f2c
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-16 17:49:38 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CarCard from "./CarCard";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import CarDetails from "./CarDetails";
+import SearchManufacturer from "./SearchManufacturer";
+import ShowMore from "./ShowMore";
+
+export {Hero,CustomButton,Navbar,Footer,SearchBar,CustomFilter,SearchManufacturer,CarCard,CarDetails,ShowMore}
